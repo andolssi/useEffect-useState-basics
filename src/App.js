8edaa24f713c0cb08cfe78deaf30ba020c25051b
Andolsi Houssem eddine <82064553+andolssi@users.noreply.github.com>
2021-04-26 01:04:54 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Setup from "./example01";
+function App() {
+  return (
+    <div className="container">
+      <Setup />
+    </div>
+  );
+}
+
+export default App;
